be05bfaba98379837e28245f60e8ed19776d9087
jamiul <jamiulewu24@gmail.com>
2021-12-28 10:21:09 +0600

    14- Destructuring Arguments

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Navbar = (props) => {
+const Navbar = ({ totalCounters }) => {
     return (
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
                 <a href="#" className="navbar-brand">
                     Navbar
-                    <span class="badge bg-dark m-2">{props.totalCounters}</span>
+                    <span class="badge bg-dark m-2">{totalCounters}</span>
                 </a>
             </div>
         </nav>
